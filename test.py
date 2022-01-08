38b816b9b308787cf14aef21bdc03cefb3c37224
piotrstani <piotr.stani@o2.pl>
2022-01-08 17:46:30 +0100

    valid ssn, valid date

--- test.py
+++ test.py
@@ -30,6 +30,12 @@ def get_date_from_ssn(ssn_str):
     return true_ssn_date
 
 
+def validate_date_format(date_str):
+    try:
+        datetime.datetime.strptime(date_str, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+
 
 def generate_ssns(elements):
     # log time
@@ -59,6 +65,9 @@ def generate_unique_ssns(elements, sex, in_start_dte, in_end_dte):
     start = datetime.now()
     # log time
 
+    validate_date_format(in_start_dte)
+    validate_date_format(in_end_dte)
+
     ssns_list = []
     while len(ssns_list) + 1 <= elements:
 
@@ -87,71 +96,105 @@ def generate_unique_ssns(elements, sex, in_start_dte, in_end_dte):
 
     return ssns_series
 
+
 def validate_ssn(ssn_str, sex, in_birth_dte):
     # TEST IS 11 INT
+    print(ssn_str)
+
+    validate_date_format(in_birth_dte)
+
     if ssn_str.isnumeric() and len(ssn_str) == 11:
-        # TEST SEX
-        check_sex = ''
-        if sex == 'dowolna':
-            check_sex = 'sex NOT CHECKED.'
-        else:
-            if sex == 'F':
-                if int(ssn_str[9]) % 2 == 0:
-                    check_sex = '1 - is female PESEL'
-                else:
-                    check_sex = '0 - is not female PESEL'
-            elif sex == 'M':
-                if int(ssn_str[9]) % 2 != 0:
-                    check_sex = '1 - is male PESEL'
-                else:
-                    check_sex = '0 - is not male PESEL'
-
-        # TEST DATE OF BIRTH
-        if in_birth_dte == 'dowolna':
-            check_dte = 'date of birth NOT CHECKED.'
-        else:
-            true_ssn_date = get_date_from_ssn(ssn_str)
-
-            birth_dte = datetime.strptime(in_birth_dte, '%Y-%m-%d')
-            if true_ssn_date == birth_dte:
-                check_dte = '1 - date of birth AS expected'
-            else:
-                check_dte = '0 - date of birth OTHER than expected'
 
         # TEST LAST DIGIT
+        check_digit_flag = ''
         weights_for_check_digit = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
         sum_digit = 0
+
         for digits in range(0, 10):
             sum_digit += weights_for_check_digit[digits] * int(ssn_str[digits])
             # print(f"{sum_digit} =  {weights_for_check_digit[digits]} * {int(ssn_str[digits])}")
 
         check_digit = 10 - sum_digit % 10
-        # print(check_digit)
-
         if check_digit == 10:
             check_digit = 0
-
-        # print(int(ssn_str[10]), check_digit)
-
-        # OUTPUTS
+        # OUTPUT
         if check_digit == int(ssn_str[10]):
-            return f"1 -Correct PESEL | {check_sex} (ex {sex}) | {check_dte} (is{true_ssn_date.date()},ex{in_birth_dte})"
+            check_digit_flag = '1'
         else:
-            return f"0 -Incorrect PESEL - incorrect control digit, {check_sex}, {check_dte}"
+            check_digit_flag = '0'
 
-    return "0 - Incorrect PESEL - must be 11 digits"
+        # TEST SEX
+        check_sex_flag = ''
+        if sex == 'dowolna':
+            check_sex_flag = 'NOT CHECKED'
+        else:
+            if sex == 'F':
+                if int(ssn_str[9]) % 2 == 0:
+                    check_sex_flag = '1' # is female PESEL'
+                else:
+                    check_sex_flag = '0' # is not female PESEL'
+            elif sex == 'M':
+                if int(ssn_str[9]) % 2 != 0:
+                    check_sex_flag = '1' # is male PESEL'
+                else:
+                    check_sex_flag = '0' # is not male PESEL'
+
+        # TEST DATE OF BIRTH
+        if in_birth_dte == 'dowolna':
+            check_dte_flag = 'NOT CHECKED'
+        else:
+            true_ssn_date = get_date_from_ssn(ssn_str)
+            birth_dte = datetime.strptime(in_birth_dte, '%Y-%m-%d')
+            if true_ssn_date == birth_dte:
+                check_dte_flag = '1' # date of birth AS expected
+            else:
+                check_dte_flag = '0' # date of birth OTHER than expected
+    # OUTPUT
+        return f"Validated {ssn_str}, logic:{check_digit_flag}, {sex}:{check_sex_flag}, {in_birth_dte}:{check_dte_flag}"
+
+    else:
+        return f"Validated {ssn_str}, PESEL - must be 11 digits"
 
 
-
-print('\nget_date_from_ssn ---------------------1')
-print(get_date_from_ssn('05250142649'))
+#print('\nget_date_from_ssn ---------------------1')
+#print(get_date_from_ssn('05250142649'))
 
 print('\ngenerate_ssns -----------------------2')
 print(generate_ssns(10))
 
 print('\ngenerate_unique_ssns F, 1900-01-01, 1999-12-31 -------------')
-print(generate_unique_ssns(10, 'F', '1900-01-01', '1999-12-31'))
-#print('\ngenerate_unique_ssns F, 1800-01-01, 1899-12-31 -------------')
-#print(generate_unique_ssns(10, 'F', '1800-01-01', '1899-12-31'))
-print('\ngenerate_unique_ssns F, 2100-01-01, 2000-12-31 -------------')
-print(generate_unique_ssns(10, 'F', '2000-01-01', '2000-12-31'))
+#print(generate_unique_ssns(10, 'F', '1900-01-01', '1999-12-31'))
+
+print('\n validate_ssn -----------------')
+#test = generate_unique_ssns(1, 'F', '2000-01-01', '2200-01-01')
+#print(test[0])
+#print(validate_ssn(test[0], 'F', '2000-01-01'))
+
+test_set0 = ['123456789012', '1234567890', '', 'abcdefghijk','1ikl..+./']
+test_set1 = ['86040198264']
+test_set2 = ['86040198260']
+
+
+
+for test in test_set1:
+    print(validate_ssn(test, 'F', '2000-01-01'))
+
+
+#print(test[1])
+
+# test_set0 = [''12345678901', '123456789012', '1234567890', '', 'abcdefghijk','1ikl..../+']
+#
+#sex
+# test1 = generate_unique_ssns(1, 'dwolna', '1900-01-01', '2100-01-01')
+# test2 = generate_unique_ssns(1, 'F, '1900-01-01', '2100-01-01')
+# test3 = generate_unique_ssns(1, 'M, '1900-01-01', '2100-01-01')
+
+
+# test3 = generate_unique_ssns(1, 'F', '1900-01-31', '1999-12-31') poza zakresie
+# test5 = generate_unique_ssns(1, 'M', '1901-02-28', '2100-12-31')
+
+
+# test4 = generate_unique_ssns(1, 'F', '1999-12-31', '1999-12-31')
+
+#for i in test.iteritems():
+#    print(i[1] + '  ' + i[1][0:6] + ' ' + i[1][9] + ' ----> ' + validate_ssn(i[1], 'M', '2005-05-01'))
